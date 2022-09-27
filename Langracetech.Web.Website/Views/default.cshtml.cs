8bb9f71de98a9790467375043ac4b9385ef74d3d
linsianting <siantingtaiwan@gmail.com>
2022-09-27 11:39:21 +0800

    再次整合新增，可以 正常運作 ( 來檢查資料庫儲存型式 )

---
+++ Langracetech.Web.Website/Views/default.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Langracetech.Web.Website.Views
+{
+    public class defaultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
